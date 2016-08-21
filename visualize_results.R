7a2e90efd7560c57908da347751fb2dcb0c2a03f
Maëlle Salmon <maelle.salmon@yahoo.se>
2016-08-21 16:55:15 +0200

    adds code for viz

--- visualize_results.R
+++ visualize_results.R
@@ -9,11 +9,23 @@ load("data/parsed_first7jobs.RData")
 
 output_with_words <- bind_cols(output, first7jobs_parsed)
 
+
+filter(output_with_words,
+       probability > 0.5) %>%
+  ggplot() +
+  geom_bar(aes(label))+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_fill_viridis(discrete = TRUE)
+ggsave(file = "count_categories.png")
+
 filter(output_with_words,
        probability > 0.5) %>%
 ggplot() +
-  geom_bar(aes(label))+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  geom_bar(aes(label, fill = as.factor(rank)),
+           position = "fill")+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_fill_viridis(discrete = TRUE)
+ggsave(file = "rank_by_category.png")
 
 # filter high probabilities
 output_with_words_high <- filter(output_with_words,
